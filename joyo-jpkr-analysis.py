81a63aebc69ebe829e33c9fa72b6bc7ba54f5158
colspan <contact@colspan.net>
2015-07-13 18:47:36 +0900

    UnixCommandTaskを実装

--- joyo-jpkr-analysis.py
+++ joyo-jpkr-analysis.py
@@ -7,6 +7,16 @@ import luigi
 import subprocess
 import sys
 
+class UnixCommandTask(luigi.Task):
+  cmdargs = None
+  def setCommand(self,cmdargs):
+    self.cmdargs = cmdargs
+  def run(self):
+    proc = subprocess.Popen(self.cmdargs,stdin=self.input().open('r'),stdout=subprocess.PIPE)
+    with self.output().open('w') as out_file:
+      ret = proc.communicate()[0]
+      print >> out_file, ret
+
 class JoyoJP(luigi.ExternalTask):
   def output(self):
     return luigi.LocalTarget('data/joyo.csv')
@@ -14,10 +24,8 @@ class JoyoJP(luigi.ExternalTask):
 class P01_ExtractKanjiKana(luigi.Task):
   def output(self):
     return luigi.LocalTarget('work/01_joyo_kanji_kana.csv')
-
   def requires(self):
     return JoyoJP()
-
   def run(self):
     with self.input().open('r') as in_file:
       with self.output().open('w') as out_file:
@@ -34,41 +42,29 @@ class P01_ExtractKanjiKana(luigi.Task):
           except ValueError, e:
             continue
 
-class P02_AppendFieldKanji2Hangul(luigi.Task):
+class P02_AppendFieldKanji2Hangul(UnixCommandTask):
   target = 'work/02_jpkr_kanji_hanja_dic_kanjihangul.csv'
+  cmdargs = ['ruby','02_append_field_kanji2hangul.rb']
   def output(self):
     return luigi.LocalTarget(self.target)
   def requires(self):
     return P01_ExtractKanjiKana()
-  def run(self):
-    proc = subprocess.Popen(['ruby','02_append_field_kanji2hangul.rb'],stdin=self.input().open('r'),stdout=subprocess.PIPE)
-    with self.output().open('w') as out_file:
-      ret = proc.communicate()[0]
-      print >> out_file, ret
 
-class P03_ParsePronunciation(luigi.Task):
+class P03_ParsePronunciation(UnixCommandTask):
   target = 'work/03_jpkr_kanji_hanja_dic_parsed_pronunciation.csv'
+  cmdargs = ['ruby','03_parse_pronunciation.rb']
   def output(self):
     return luigi.LocalTarget(self.target)
   def requires(self):
     return P02_AppendFieldKanji2Hangul()
-  def run(self):
-    proc = subprocess.Popen(['ruby','03_parse_pronunciation.rb'],stdin=self.input().open('r'),stdout=subprocess.PIPE)
-    with self.output().open('w') as out_file:
-      ret = proc.communicate()[0]
-      print >> out_file, ret
 
-class P04_CleansingAndAppendLabels(luigi.Task):
+class P04_CleansingAndAppendLabels(UnixCommandTask):
   target = 'work/04_jpkr_kanji_hanja_dic.csv'
+  cmdargs = ['ruby','04_cleansing_append_labels.rb']
   def output(self):
     return luigi.LocalTarget(self.target)
   def requires(self):
     return P03_ParsePronunciation()
-  def run(self):
-    proc = subprocess.Popen(['ruby','04_cleansing_append_labels.rb'],stdin=self.input().open('r'),stdout=subprocess.PIPE)
-    with self.output().open('w') as out_file:
-      ret = proc.communicate()[0]
-      print >> out_file, ret
 
 if __name__ == '__main__':
   luigi.run(main_task_cls=P04_CleansingAndAppendLabels,local_scheduler=True )
